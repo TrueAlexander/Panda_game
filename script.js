9c5a4bda3e7ff3792815a1564c32a44f5c248289
Alexander <caminante.msk@gmail.com>
2022-01-09 14:57:26 +0300

    added modal window

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const panda = document.getElementById('panda');
 const bamboo = document.getElementById('bamboo');
-document.addEventListener('keydown', function (event) {
-  jump();
-})
+const body = document.querySelector('body');
+const game = document.querySelector('.game');
 
-function jump() {
-  if (panda.classList != 'jump') {
-    panda.classList.add('jump');
-  }
+
+document.addEventListener('keydown', function (event) {
+  panda.classList.add('jump');
   setTimeout(function () {
     panda.classList.remove('jump')
-  }, 800)
-
-}
+  }, 500)
+})
 
 let isAlive = setInterval(function () {
   let pandaTop = parseInt(window.getComputedStyle(panda).getPropertyValue('top'))
   let bambooLeft = parseInt(window.getComputedStyle(bamboo).getPropertyValue('left'))
 
-  if (bambooLeft < 80 && bambooLeft > 10 && pandaTop >= 450) {
-    alert('Game Over!')
+  if (bambooLeft < 80 && bambooLeft > 10 && pandaTop >= 300) {
+
+    bamboo.style.animation = 'none'
+    bamboo.style.left = '80px'
+    panda.classList.remove('swing')
+
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = '<h1>Game over!!</h1>'
+    game.append(modal);
+
   }
-}, 50);
+}, 10);
