a22a69edbadba66b3de5020aef36ca9dff0a345c
Alexander <caminante.msk@gmail.com>
2022-01-10 14:54:10 +0300

    adaptive experiments

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const panda = document.getElementById('panda');
 const bamboo = document.getElementById('bamboo');
+const cloud1 = document.getElementById('cloud1');
+const cloud2 = document.getElementById('cloud2');
 const body = document.querySelector('body');
 const game = document.querySelector('.game');
-
+const btnUp = document.querySelector('.up-btn');
 
 document.addEventListener('keydown', function (event) {
+  panda.classList.add('jump');
+  setTimeout(function () {
+    panda.classList.remove('jump')
+  }, 500)
+})
+
+btnUp.addEventListener('click', () => {
   panda.classList.add('jump');
   setTimeout(function () {
     panda.classList.remove('jump')
@@ -15,16 +24,27 @@ let isAlive = setInterval(function () {
   let pandaTop = parseInt(window.getComputedStyle(panda).getPropertyValue('top'))
   let bambooLeft = parseInt(window.getComputedStyle(bamboo).getPropertyValue('left'))
 
-  if (bambooLeft < 80 && bambooLeft > 10 && pandaTop >= 300) {
+  if (bambooLeft < 80 && bambooLeft > 10 && pandaTop >= 100) {
+
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = '<h1>Game over!!</h1><button class="btn restart-btn" type="button">Restart</button>'
+    game.append(modal);
 
     bamboo.style.animation = 'none'
     bamboo.style.left = '80px'
     panda.classList.remove('swing')
 
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.innerHTML = '<h1>Game over!!</h1>'
-    game.append(modal);
+    cloud1.style.opacity = '0'
+    cloud2.style.opacity = '0'
+    const btnRestart = modal.querySelector('.btn');
+    btnUp.disabled = 'true'
+    console.dir(btnUp);
+
+    btnRestart.addEventListener('click', () => {
+      window.location.reload()
+    })
+
 
   }
 }, 10);
